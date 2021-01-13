bd01a4c6ec85784c50f597245bdcf423925c384f
nhans32 <56236772+nhans32@users.noreply.github.com>
2021-01-12 18:19:11 -0800

    add name parse

--- sample_backend.py
+++ sample_backend.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -38,5 +39,12 @@ users = {
 
 @app.route('/users')
 def get_users():
+   search_username = request.args.get('name') #accessing the value of parameter 'name'
+   if search_username :
+      subdict = {'users_list' : []}
+      for user in users['users_list']:
+         if user['name'] == search_username:
+            subdict['users_list'].append(user)
+      return subdict
    return users
 
